9fec273e39db645309f1ca89a6cdc4eb3447a0a0
agapinank <agapinank@gmail.com>
2021-10-23 17:00:59 -0700

    my react app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+//component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+import React from "react";
+
+const Header =()=>{
+    return <h1 >The Keeper App</h1>;
+}
+
+
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+//to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+import React from "react";
+
+const Note =()=>{
+    return(
+        <div>
+        <h1> Title</h1>
+            <p>
+                This is the note content.
+            </p>
+        </div>
+    )
+}
+
+export default Note;
